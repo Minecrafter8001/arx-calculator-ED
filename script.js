457dafd098d5b259553090a8139004196003fd24
minecrafter8001 <133237713+Minecrafter8001@users.noreply.github.com>
2024-09-02 10:53:01 +0100

    changed theme to darkmode

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Apply the saved theme immediately
+    applyDarkMode();
+
+    // Reveal content after the theme has been applied
+    setTimeout(() => {
+        document.body.style.visibility = 'visible';
+    }, 0);
+});
+
+// Function to apply dark mode
+function applyDarkMode() {
+    // Apply dark mode as default
+    document.body.classList.add('dark-mode');
+}
+
+
+
 document.getElementById("arx-form").addEventListener("submit", function(event) {
     event.preventDefault();
 
     const targetArx = parseInt(document.getElementById("target_arx").value);
     let currentArx = parseInt(document.getElementById("current_arx").value);
     const currency = document.getElementById("currency").value;
+
     if (isNaN(currentArx)) {
         currentArx = 0;
     }
@@ -11,12 +30,12 @@ document.getElementById("arx-form").addEventListener("submit", function(event) {
     const arxNeeded = targetArx - currentArx;
 
     const packs = [
-        {arx: 100000, price: {GBP: 44.99, EUR: 54.99, USD: 59.99}},
-        {arx: 54000, price: {GBP: 24.99, EUR: 29.99, USD: 37.99}},
-        {arx: 26800, price: {GBP: 12.99, EUR: 15.99, USD: 18.99}},
-        {arx: 17700, price: {GBP: 9.59, EUR: 11.49, USD: 12.99}},
-        {arx: 8820, price: {GBP: 4.99, EUR: 5.99, USD: 6.99}},
-        {arx: 5000, price: {GBP: 2.99, EUR: 3.49, USD: 3.99}}
+        { arx: 100000, price: { GBP: 44.99, EUR: 54.99, USD: 59.99 } },
+        { arx: 54000, price: { GBP: 24.99, EUR: 29.99, USD: 37.99 } },
+        { arx: 26800, price: { GBP: 12.99, EUR: 15.99, USD: 18.99 } },
+        { arx: 17700, price: { GBP: 9.59, EUR: 11.49, USD: 12.99 } },
+        { arx: 8820, price: { GBP: 4.99, EUR: 5.99, USD: 6.99 } },
+        { arx: 5000, price: { GBP: 2.99, EUR: 3.49, USD: 3.99 } }
     ];
 
     let minCost = Infinity;
@@ -24,13 +43,10 @@ document.getElementById("arx-form").addEventListener("submit", function(event) {
     let leftoverArx = 0;
 
     for (let r = 1; r <= 10; r++) {
-        combinationsWithReplacement(packs, r).forEach(function(combination) {
+        combinationsWithReplacement(packs, r).forEach(function (combination) {
             const totalArx = combination.reduce((sum, pack) => sum + pack.arx, 0);
             const totalCost = combination.reduce((sum, pack) => {
-                if (pack.price[currency] !== undefined) {
-                    return sum + pack.price[currency];
-                }
-                return sum;
+                return sum + (pack.price[currency] || 0);
             }, 0);
 
             if (totalArx >= arxNeeded && totalCost < minCost) {
